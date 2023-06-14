dbbe5681407a1520a3725e536931b2d8872fd51e
how-dev <howard.medeiros@gmail.com>
2023-06-14 08:14:26 -0300

    feat: Add SMTCMMS Administration on panel

--- core_api/urls.py
+++ core_api/urls.py
@@ -5,6 +5,10 @@ from user.urls import *  # noqa
 from customer.urls import *  # noqa
 from machine.urls import *  # noqa
 
+admin.site.site_header = "SMTCMMS Administation"
+admin.site.site_title = "SMTCMMS Administation"
+admin.site.index_title = "SMTCMMS Administation"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include(router.urls)),  # noqa: F405
