7c0df955a8410b4a291fd1663736bc4f0a7d31c6
how-dev <howard.medeiros@gmail.com>
2023-06-24 00:31:37 -0300

    fix: Open /states on customer routes

--- customer/permissions.py
+++ customer/permissions.py
@@ -3,6 +3,9 @@ from rest_framework.permissions import BasePermission
 
 class CustomerPermissions(BasePermission):
     def has_permission(self, request, _):
+        if "/states" in request.path:
+            return True
+
         user = request.user
 
         if not user.is_superuser:
